92fc0a60d86cb830a4cc4aaa8b44ab30defb6179
HiteshMSR <msrhitesh@gmail.com>
2022-06-13 16:42:01 +0530

    Message List

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data=[
+    { name:"Hitesh", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkWUSSBG0cSo-YZjdsB5TOhjNWQeBaEAj4g&usqp=CAU"},
+    {name:"Abhishek", img:"https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg"}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map( (obj) => (<Message name={obj.name} img={obj.img} />))}
     </div>
   );
 }
 
 export default App;
+
+
+
+function Message(props) {
+  return (
+  <div>
+   <img className="profile-pic" src={props.img} />
+    <h1> Hello, {props.name}</h1>
+  </div>
+  
+  )
+}

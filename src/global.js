0ce08c312400ebf784e1e4db896a934da20d5bf0
HiteshMSR <96234432+HiteshMSR@users.noreply.github.com>
2022-07-19 10:44:09 +0530

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,3 +1,4 @@
 // export const API=`https://61ea3b297bc0550017bc660c.mockapi.io/`;
 
- export const API=`https://hitesh-node-app.herokuapp.com`;
+// export const API=`https://hitesh-node-app.herokuapp.com`;
+export const API=`http://localhost:4000/`;
